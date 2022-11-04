dd2a2987b783b4a192a026099e54bd3005a1d116
Karen <karentomedi@gmail.com>
2022-11-04 06:31:02 -0300

    mais testes

--- js/ShowDoMilhao.js
+++ js/ShowDoMilhao.js
@@ -1,10 +1,10 @@
 // CLASSE
 
-class Game {
+class Jogo {
   constructor() {
-    this.round = 1;
+    this.rodada = 1;
     this.premio = [
-      0,
+      "R$ 0,00",
       "R$ 50.000,00",
       "R$ 100.000,00",
       "R$ 250.000,00",
@@ -16,20 +16,20 @@ class Game {
       {
         pergunta:
           "Que animal da fauna brasileira está retratado na nota de 10 reais?",
-        alternativas: ["1) Jabuti", "2) Onça", "3) Arara", "4) Tucano"],
-        resposta: "3) Arara",
+        alternativas: ["1) Jabuti", "3) Onça", "2) Arara", "4) Tucano"],
+        resposta: "2) Arara",
       },
       {
         pergunta: "Quingentésimo corresponde a qual número?",
-        alternativas: ["1) 500", "2) 5.000", "3) 50", "4) 5"],
+        alternativas: ["1) 500", "3) 5.000", "2) 50", "4) 5"],
         resposta: "1) 500",
       },
       {
         pergunta: "Que profissional usa uma ferramenta chamada formão?",
         alternativas: [
           "1) Carpinteiro",
-          "2) Relojoeiro",
-          "3) Confeiteiro",
+          "3) Relojoeiro",
+          "2) Confeiteiro",
           "4) Bombeiro",
         ],
         resposta: "1) Carpinteiro",
@@ -38,11 +38,26 @@ class Game {
         pergunta: "Quando começou e terminou a Primeira Guerra Mundial? ",
         alternativas: [
           "1) 1914 - 1918",
-          "2) 1902 - 1908 ",
-          "3) 1920 - 1930",
+          "3) 1902 - 1908 ",
+          "2) 1920 - 1930",
           "4) 1912 - 1915",
         ],
         resposta: "1) 1914 - 1918",
+      },
+      {
+        pergunta: "Qual bicho transmite a doença de Chagas?",
+        alternativas: ["1) Abelha", "3) Barata", "2) Pulga", "4) Barbeiro"],
+        resposta: "4) Barbeiro",
+      },
+      {
+        pergunta: "A jogada chamada 'strike' é feita em qual jogo?",
+        alternativas: [
+          "1) Tênis de mesa",
+          "3) Beisebol",
+          "2) Xadrez",
+          "4) Boliche",
+        ],
+        resposta: "4) Boliche",
       },
     ];
     this.perguntasNivel2 = [
@@ -50,109 +65,183 @@ class Game {
         pergunta: "O samba é uma dança originária de qual continente?",
         alternativas: [
           "1) Europa",
-          "2) Ásia",
-          "3) África",
+          "3) Ásia",
+          "2) África",
           "4) América do Sul",
         ],
-        resposta: "3) África",
+        resposta: "2) África",
+      },
+      {
+        pergunta: "Qual é o triângulo que tem todos os lados diferentes?",
+        alternativas: [
+          "1) Equilátero",
+          "3) Isósceles",
+          "2) Escaleno",
+          "4) Trapézio",
+        ],
+        resposta: "2) Escaleno",
+      },
+      {
+        pergunta: "Qual é o coletivo de cães?",
+        alternativas: ["1) Matilha", "3) Rebanho", "2) Alcatéia", "4) Manada"],
+        resposta: "1) Matilha",
       },
       {
         pergunta: "Como se chama o estudo entre palavras e significados?",
         alternativas: [
           "1) Fonética",
-          "2) Semântica",
-          "3) Filosofia",
+          "3) Semântica",
+          "2) Filosofia",
           "4) Psiquiatria",
         ],
-        resposta: "2) Semântica",
+        resposta: "3) Semântica",
       },
       {
         pergunta:
           "Como se chama o território compreendido entre o istmo do Panamá e Terra do Fogo?",
         alternativas: [
           "1) América do Norte",
-          "2) América Central",
-          "3) América do Sul",
+          "3) América Central",
+          "2) América do Sul",
           "4) América Latina",
         ],
-        resposta: "3) América do Sul",
+        resposta: "2) América do Sul",
       },
       {
         pergunta:
           "Qual flor é o símbolo nacional do Egito e foi considerada sagrada na antiguidade?",
-        alternativas: ["1) Rosa", "2) Lírio", "3) Jasmim", "4) Lótus"],
+        alternativas: ["1) Rosa", "3) Lírio", "2) Jasmim", "4) Lótus"],
         resposta: "4) Lótus",
       },
     ];
     this.perguntasNivel3 = [
       {
-        pergunta: "Qual o nome da esposa do escritor Jorge Amado",
+        pergunta: "Qual o antônimo de malograr?",
         alternativas: [
-          "1) Zélia Cardoso de Melo",
-          "Zélia Gattai",
-          "3) Zélia Ribeiro",
-          "4) Zélia de Almeida",
+          "1) Perder",
+          "3) Fracassar",
+          "2) Conseguir",
+          "4) Desprezar",
         ],
-        resposta: "alternativa 1",
+        resposta: "2) Conseguir",
       },
       {
-        pergunta: "pergunta 3-2",
-        alternativas: ["alternativa 1", "alternativa 2"],
-        resposta: "alternativa 1",
+        pergunta: "O adjetivo venoso está relacionado a:",
+        alternativas: ["1) Vela", "3) Vento", "2) Vênia", "4) Veia"],
+        resposta: "4) Veia",
       },
       {
-        pergunta: "pergunta 3-3",
-        alternativas: ["alternativa 1", "alternativa 2"],
-        resposta: "alternativa 1",
+        pergunta: "Montezuma era o líder de qual povo?",
+        alternativas: ["1) Maia", "3) Asteca", "2) Inca", "4) Guarani"],
+        resposta: "3) Asteca",
       },
       {
-        pergunta: "pergunta 3-4",
-        alternativas: ["alternativa 1", "alternativa 2"],
-        resposta: "alternativa 1",
+        pergunta:
+          "Quem dirigiu o filme Central do Brasil, indicado ao Oscar de Melhor Filme Estrangeiro em 1999?",
+        alternativas: [
+          "1) Bruno Barreto",
+          "3) Hector Babenco",
+          "2) Arnaldo Jabor",
+          "4) Walter Salles Jr.",
+        ],
+        resposta: "4) Walter Salles Jr.",
+      },
+      {
+        pergunta: "Quem atravessou a cidade nua montada em um cavalo branco?",
+        alternativas: [
+          "1) Lady MacBeth",
+          "3) Lady Diana",
+          "2) Lady Godiva",
+          "4) Lady Hawk",
+        ],
+        resposta: "2) Lady Godiva",
+      },
+      {
+        pergunta: "Em que parte do corpo se encontra a epiglote?",
+        alternativas: ["1) Estômago", "3) Pâncreas", "2) Rim", "4) Boca"],
+        resposta: "4) Boca",
       },
     ];
     this.perguntasNivel4 = [
       {
-        pergunta: "pergunta 4-1",
-        alternativas: ["alternativa 1", "alternativa 2"],
-        resposta: "alternativa 1",
+        pergunta: "Em que país nasceu Carmem Miranda?",
+        alternativas: [
+          "1) Brasil",
+          "3) Espanha",
+          "2) Portugal",
+          "4) Argentina",
+        ],
+        resposta: "2) Portugal",
       },
       {
-        pergunta: "pergunta 4-2",
-        alternativas: ["alternativa 1", "alternativa 2"],
-        resposta: "alternativa 1",
+        pergunta: "A compensação por perda é chamada de:",
+        alternativas: [
+          "1) Déficit",
+          "3) Indenização",
+          "2) Indexação",
+          "4) Indébito",
+        ],
+        resposta: "3) Indenização",
       },
       {
-        pergunta: "pergunta 4-3",
-        alternativas: ["alternativa 1", "alternativa 2"],
-        resposta: "alternativa 1",
+        pergunta:
+          "Qual montanha se localiza na fronteira do Tibet com o Nepal?",
+        alternativas: [
+          "1) Monte Everest",
+          "3) Monte Carlo",
+          "2) Monte Fuji",
+          "4) Monte Branco",
+        ],
+        resposta: "1) Monte Everest",
       },
       {
-        pergunta: "pergunta 4-4",
-        alternativas: ["alternativa 1", "alternativa 2"],
-        resposta: "alternativa 1",
+        pergunta: "Quem compôs a melodia do Hino da Independência?",
+        alternativas: [
+          "1) Dom Pedro I",
+          "3) Manuel Bandeira",
+          "2) Castro Alves",
+          "4) Carlos Gomes",
+        ],
+        resposta: "1) Dom Pedro I",
+      },
+      {
+        pergunta:
+          "Seguindo a sequência do baralho, qual carta vem depois do dez?",
+        alternativas: ["1) Rei", "3) Valete", "2) Nove", "4) Ás"],
+        resposta: "3) Valete",
       },
     ];
     this.perguntasNivel5 = [
       {
-        pergunta: "pergunta 5-1",
-        alternativas: ["alternativa 1", "alternativa 2"],
-        resposta: "alternativa 1",
+        pergunta: "Qual era a nacionalidade do escritor Oscar Wilde?",
+        alternativas: ["1) Irlandês", "3) Alemão", "2) Inglês", "4) Americano"],
+        resposta: "1) Irlandês",
       },
       {
-        pergunta: "pergunta 5-2",
-        alternativas: ["alternativa 1", "alternativa 2"],
-        resposta: "alternativa 1",
+        pergunta: "Qual é o primeiro nome do pintor Cezanne?",
+        alternativas: ["1) Jean", "3) Paul", "2) Georges", "4) Jacques"],
+        resposta: "3) Paul",
       },
       {
-        pergunta: "pergunta 5-3",
-        alternativas: ["alternativa 1", "alternativa 2"],
-        resposta: "alternativa 1",
+        pergunta: "Que nome se dá à purificação por meio da água?",
+        alternativas: [
+          "1) Abolição",
+          "3) Abnegação",
+          "2) Ablução",
+          "4) Abrupção",
+        ],
+        resposta: "2) Ablução",
       },
       {
-        pergunta: "pergunta 5-4",
-        alternativas: ["alternativa 1", "alternativa 2"],
-        resposta: "alternativa 1",
+        pergunta:
+          "Qual foi o último presidente no período da ditadura militar no Brasil?",
+        alternativas: [
+          "1) Costa e Silva",
+          "3) João Figueiredo",
+          "2) Ernesto Geisel",
+          "4) Emílio Médici",
+        ],
+        resposta: "3) João Figueiredo",
       },
     ];
   }
@@ -163,15 +252,25 @@ class Game {
     const alternativa2 = document.getElementById("botaoAlternativa2");
     const alternativa3 = document.getElementById("botaoAlternativa3");
     const alternativa4 = document.getElementById("botaoAlternativa4");
+    const botaoErrar = document.getElementById("botaoErrar");
+    const botaoParar = document.getElementById("botaoParar");
+    const botaoAcertar = document.getElementById("botaoAcertar");
+    const desfechosPossiveis = document.querySelector(".desfechosPossiveis");
+    const mensagemStatus = document.getElementById("mensagemStatus");
     alternativa1.className = "botaoResposta";
     alternativa2.className = "botaoResposta";
     alternativa3.className = "botaoResposta";
     alternativa4.className = "botaoResposta";
-    valorPremio.innerHTML = this.premio[this.round];
+    alternativa1.disable = false;
+    alternativa2.disable = false;
+    alternativa3.disable = false;
+    alternativa4.disable = false;
+    valorPremio.innerHTML = this.premio[this.rodada];
+    valorAtual.innerHTML = this.premio[this.rodada - 1];
 
     let sorteioIndicePergunta;
 
-    if (this.round === 1) {
+    if (this.rodada === 1) {
       sorteioIndicePergunta = Math.floor(
         Math.random() * this.perguntasNivel1.length
       );
@@ -185,11 +284,14 @@ class Game {
         this.perguntasNivel1[sorteioIndicePergunta].alternativas[2];
       alternativa4.textContent =
         this.perguntasNivel1[sorteioIndicePergunta].alternativas[3];
+      botaoErrar.textContent = "Se ERRAR você não ganha nada";
+      botaoParar.textContent = "Se PARAR agora você não ganha nada";
+      botaoAcertar.textContent = "Se ACERTAR você ganha R$ 50.000,00";
       respostaCerta = this.perguntasNivel1[sorteioIndicePergunta].resposta;
       return this.perguntasNivel1[sorteioIndicePergunta];
     }
 
-    if (this.round === 2) {
+    if (this.rodada === 2) {
       sorteioIndicePergunta = Math.floor(
         Math.random() * this.perguntasNivel2.length
       );
@@ -203,11 +305,14 @@ class Game {
         this.perguntasNivel2[sorteioIndicePergunta].alternativas[2];
       alternativa4.textContent =
         this.perguntasNivel2[sorteioIndicePergunta].alternativas[3];
+      botaoErrar.textContent = "Se ERRAR você ganha R$ 25.000,00";
+      botaoParar.textContent = "Se PARAR agora você ganha R$ 50.000,00";
+      botaoAcertar.textContent = "Se ACERTAR você ganha R$ 100.000,00";
       respostaCerta = this.perguntasNivel2[sorteioIndicePergunta].resposta;
       return this.perguntasNivel2[sorteioIndicePergunta];
     }
 
-    if (this.round === 3) {
+    if (this.rodada === 3) {
       sorteioIndicePergunta = Math.floor(
         Math.random() * this.perguntasNivel3.length
       );
@@ -221,11 +326,14 @@ class Game {
         this.perguntasNivel3[sorteioIndicePergunta].alternativas[2];
       alternativa4.textContent =
         this.perguntasNivel3[sorteioIndicePergunta].alternativas[3];
+      botaoErrar.textContent = "Se ERRAR você ganha R$ 50.000,00";
+      botaoParar.textContent = "Se PARAR agora você ganha R$ 100.000,00";
+      botaoAcertar.textContent = "Se ACERTAR você ganha R$ 250.000,00";
       respostaCerta = this.perguntasNivel3[sorteioIndicePergunta].resposta;
       return this.perguntasNivel3[sorteioIndicePergunta];
     }
 
-    if (this.round === 4) {
+    if (this.rodada === 4) {
       sorteioIndicePergunta = Math.floor(
         Math.random() * this.perguntasNivel4.length
       );
@@ -239,11 +347,14 @@ class Game {
         this.perguntasNivel4[sorteioIndicePergunta].alternativas[2];
       alternativa4.textContent =
         this.perguntasNivel4[sorteioIndicePergunta].alternativas[3];
+      botaoErrar.textContent = "Se ERRAR você ganha R$ 125.000,00";
+      botaoParar.textContent = "Se PARAR agora você ganha R$ 250.000,00";
+      botaoAcertar.textContent = "Se ACERTAR você ganha R$ 500.000,00";
       respostaCerta = this.perguntasNivel4[sorteioIndicePergunta].resposta;
       return this.perguntasNivel4[sorteioIndicePergunta];
     }
 
-    if (this.round === 5) {
+    if (this.rodada === 5) {
       sorteioIndicePergunta = Math.floor(
         Math.random() * this.perg5ntasNivel5.length
       );
@@ -257,35 +368,88 @@ class Game {
         this.perguntasNivel5[sorteioIndicePergunta].alternativas[2];
       alternativa4.textContent =
         this.perguntasNivel5[sorteioIndicePergunta].alternativas[3];
+      botaoErrar.textContent = "Se ERRAR você ganha R$ 125.000,00";
+      botaoParar.textContent = "Se PARAR agora você ganha R$ 500.000,00";
+      botaoAcertar.textContent = "Se ACERTAR você ganha R$ 1.000.000,00";
       respostaCerta = this.perguntasNivel5[sorteioIndicePergunta].resposta;
       return this.perguntasNivel5[sorteioIndicePergunta];
     }
   }
 
-  validarResposta(respostaClicada, game) {
+  validarResposta(respostaClicada, jogo) {
     const botaoContinuar = document.querySelector("#botaoContinuar");
     const botaoJogarNovamente = document.querySelector("#botaoJogarNovamente");
+    const cadaBotao = document.querySelectorAll(".botaoResposta");
+    const botaoParar = document.getElementById("botaoParar");
+    const botaoAcertar = document.getElementById("botaoAcertar");
+    const desfechosPossiveis = document.querySelector(".desfechosPossiveis");
+    const status = document.querySelector(".status");
+    const mensagemStatus = document.querySelector("#mensagemStatus");
+    const telaDeJogo = document.getElementById("telaDeJogo");
 
-    if (this.round === 5 && respostaClicada.textContent === respostaCerta) {
-      window.alert("Você ganhou!");
-      window.location.reload();
-    }
+    if(this.rodada === 5){
+      if(respostaClicada.textContent === respostaCerta){
+        respostaClicada.classList.add("verde");
+        status.classList.remove("hide");
+        mensagemStatus.classList.remove("hide");
+        desfechosPossiveis.classList.add("hide");
+        botaoJogarNovamente.classList.remove("hide");
+        mensagemStatus.innerHTML = "Parabéns! Você ganhou um milhão!";
+      } else {
+        respostaClicada.classList.add("vermelho");
+        status.classList.remove("hide");
+        mensagemStatus.classList.remove("hide");
+        desfechosPossiveis.classList.add("hide");
+        botaoJogarNovamente.classList.remove("hide");
+        mensagemStatus.innerHTML = "Ah que pena! Você perdeu tudo!";
+      }
+    } 
 
-    if (respostaClicada.textContent === respostaCerta) {
-      respostaClicada.classList.add("verde");
-      botaoContinuar.classList.remove("hide");
-    }
-
-    if (respostaClicada.textContent !== respostaCerta) {
-      respostaClicada.classList.add("vermelho");
-      game.perguntasNivel1.alternativas;
-      botaoJogarNovamente.classList.remove("hide");
-    }
-  }
-
-  pickQuestion() {
-    if (this.round === 1) {
-      this.perguntaAtual = this.perguntasNivel1[0];
+    if(this.rodada !== 5){
+      if(respostaClicada.textContent === respostaCerta){
+        respostaClicada.classList.add("verde");
+        status.classList.remove("hide");
+        mensagemStatus.classList.remove("hide");
+        desfechosPossiveis.classList.add("hide");
+        botaoContinuar.classList.remove("hide");
+        mensagemStatus.innerHTML = "Parabéns! Você acertou!";
+      } else {
+        respostaClicada.classList.add("vermelho");
+        status.classList.remove("hide");
+        mensagemStatus.classList.remove("hide");
+        desfechosPossiveis.classList.add("hide");
+        botaoJogarNovamente.classList.remove("hide");
+        mensagemStatus.innerHTML = "Ah que pena! Você perdeu!";
+      }
     }
   }
 }
+    
+
+    /*if (respostaClicada.textContent === respostaCerta) {
+      respostaClicada.classList.add("verde");
+      botaoContinuar.classList.remove("hide");
+      mensagemStatus.innerHTML =
+        "Você acumulou " + jogo.premio[jogo.rodada] + "!";
+      desfechosPossiveis.classList.remove("hide");
+      if (this.rodada === 5) {
+        botaoContinuar.classList("hide");
+        mensagemStatus.innerHTML =
+          "Parabéns! Você ganhou um milhão de reais!!!";
+      }
+    } else {
+      respostaClicada.classList.add("vermelho");
+      botaoContinuar.classList.add("hide");
+      botaoJogarNovamente.classList.remove("hide");
+      mensagemStatus.innerHTML = "Você errou! Você acumulou o prêmio XXX";
+      if (this.rodada === 5) {
+        mensagemStatus.innerHTML = "Ah não! Você errou e perdeu tudo!";
+      }
+    }
+
+    [].forEach.call(cadaBotao, function (botao, i) {
+      botao.disable = true;
+      if (cadaBotao[i].textContent === respostaCerta) {
+        botao.classList.add("verde");
+      }
+    });*/
--- js/index.js
+++ js/index.js
@@ -1,86 +1,74 @@
 // MANIPULAÇÃO DO DOM
 
-const startScreen = document.getElementById("startScreen");
-const gameScreen = document.getElementById("gameScreen");
-const gameScreenTop = document.getElementById("gameScreenTop");
-const pergunta = document.getElementById("pergunta");
-const respostas = document.getElementById("respostas");
-const gameScreenBottom = document.getElementById("gameScreenBottom");
-const status = document.getElementById("status");
-
+const telaInicial = document.querySelector(".telaInicial");
+const telaDeJogo = document.querySelector(".telaDeJogo");
+//const telaDeJogoParteSuperior = document.getElementById("telaDeJogoParteSuperior");
+//const pergunta = document.getElementById("pergunta");
+//const respostas = document.getElementById("respostas");
+//const telaDeJogoParteInferior = document.getElementById("telaDeJogoParteInferior");
+//const status = document.getElementById("status");
 const botaoJogar = document.getElementById("botaoJogar");
-const botaoErrar = document.getElementById("botaoErrar");
-const botaoParar = document.getElementById("botaoParar");
-const botaoAcertar = document.getElementById("botaoAcertar");
+//const botaoErrar = document.getElementById("botaoErrar");
+//const botaoParar = document.getElementById("botaoParar");
+//const botaoAcertar = document.getElementById("botaoAcertar");
 const botaoContinuar = document.getElementById("botaoContinuar");
 const botaoJogarNovamente = document.getElementById("botaoJogarNovamente");
 const botaoAlternativa1 = document.getElementById("botaoAlternativa1");
 const botaoAlternativa2 = document.getElementById("botaoAlternativa2");
 const botaoAlternativa3 = document.getElementById("botaoAlternativa3");
 const botaoAlternativa4 = document.getElementById("botaoAlternativa4");
-
 const valorPremio = document.getElementById("valorPremio");
 const valorAtual = document.getElementById("valorAtual");
-const valorSeErrar = document.getElementById("valorSeErrar");
-const valorSeParar = document.getElementById("valorSeParar");
-const valorSeAcertar = document.getElementById("valorSeContinuar");
-const mensagemStatus = document.getElementById("mensagemStatus");
-const controlesEmJogo = document.querySelector(".controlesEmJogo");
-const controlesFinalJogo = document.querySelector(".controlesFinalJogo");
+//const valorSeErrar = document.getElementById("valorSeErrar");
+//const valorSeParar = document.getElementById("valorSeParar");
+//const valorSeAcertar = document.getElementById("valorSeContinuar");
+//const mensagemStatus = document.getElementById("mensagemStatus");
+const desfechosPossiveis = document.querySelector(".desfechosPossiveis");
+//const validacao = document.querySelector(".validacao");
 
-const startGame = new Game();
+const novoJogo = new Jogo();
 
 let respostaCerta = "";
 
-botaoJogar.addEventListener("click",()=>{
-  startScreen.classList.add ("hide");
-  gameScreen.classList.remove("hide");
-  startGame.perguntaAtual = startGame.showQuestion();
+botaoJogar.addEventListener("click", () => {
+  telaInicial.classList.add("hide");
+  telaDeJogo.classList.remove("hide");
+  novoJogo.perguntaAtual = novoJogo.showQuestion();
 });
 
-botaoJogarNovamente.addEventListener("click", ()=>{
-  startScreen.classList.remove ("hide");
-  gameScreen.classList.add("hide");
-  controlesEmJogo.classList.remove("hide");
+botaoJogarNovamente.addEventListener("click", () => {
+  telaInicial.classList.remove("hide");
+  telaDeJogo.classList.add("hide");
+  desfechosPossiveis.classList.remove("hide");
   botaoJogarNovamente.classList.add("hide");
-})
+  mensagemStatus.classList.add("hide");
+  Jogo.rodada === 1;
+});
 
-botaoAlternativa1.addEventListener("click", ()=>{
-  startGame.validarResposta(botaoAlternativa1, startGame);
-  controlesEmJogo.classList.add("hide");
+botaoAlternativa1.addEventListener("click", () => {
+  novoJogo.validarResposta(botaoAlternativa1, novoJogo);
+  desfechosPossiveis.classList.add("hide");
+});
 
-})
+botaoAlternativa2.addEventListener("click", () => {
+  novoJogo.validarResposta(botaoAlternativa2, novoJogo);
+  desfechosPossiveis.classList.add("hide");
+});
 
-botaoAlternativa2.addEventListener("click", ()=>{
-  startGame.validarResposta(botaoAlternativa2, startGame);
-  controlesEmJogo.classList.add("hide");
-})
+botaoAlternativa3.addEventListener("click", () => {
+  novoJogo.validarResposta(botaoAlternativa3, novoJogo);
+  desfechosPossiveis.classList.add("hide");
+});
 
-botaoAlternativa3.addEventListener("click", ()=>{
-  startGame.validarResposta(botaoAlternativa3, startGame);
-  controlesEmJogo.classList.add("hide");
-})
+botaoAlternativa4.addEventListener("click", () => {
+  novoJogo.validarResposta(botaoAlternativa4, novoJogo);
+  desfechosPossiveis.classList.add("hide");
+});
 
-botaoAlternativa4.addEventListener("click", ()=>{
-  startGame.validarResposta(botaoAlternativa4, startGame);
-  controlesEmJogo.classList.add("hide");
-})
-
-botaoContinuar.addEventListener("click", ()=>{
-  startGame.round ++
-  startGame.perguntaAtual = startGame.showQuestion();
-  controlesEmJogo.classList.remove("hide");
+botaoContinuar.addEventListener("click", () => {
+  novoJogo.rodada++;
+  novoJogo.perguntaAtual = novoJogo.showQuestion();
+  desfechosPossiveis.classList.remove("hide");
   botaoContinuar.classList.add("hide");
-})
-
-
-
-
-/*const alternativa1 = document.querySelector("#alternativa1");
-  const corretas = ["50", "200"];
-  alternativa1.addEventListener("click", () => {
-    console.log(corretas.includes(alternativa1.innerText));
-    if (corretas.includes(alternativa1.innerText)) {
-      console.log("acertou!");
-    }
-  });*/
+  mensagemStatus.classList.add("hide");
+});
